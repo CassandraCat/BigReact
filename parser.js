b27d3ee50d43e1031975ee7d8ed769e06a0ba861
CrazyCatZhang <crazycatzhang@outlook.com>
2024-05-30 14:39:56 +0800

    🐛 fix: use require in js-module

--- parser.js
+++ parser.js
@@ -1,13 +1,12 @@
 // parser.js
-const emojiRegex = require('emoji-regex');
-
 module.exports = async (message, opts = {}) => {
-  const { default: parser } = await import('@commitlint/parse');
-  const match = emojiRegex().exec(message);
-  const msg = match ? message.replace(match[0], '').trim() : message;
-  const result = await parser(msg, opts);
-  return {
-    ...result,
-    raw: message, // Keep the original message
-  };
+	const emojiRegex = await import('emoji-regex');
+	const { default: parser } = await import('@commitlint/parse');
+	const match = emojiRegex().exec(message);
+	const msg = match ? message.replace(match[0], '').trim() : message;
+	const result = await parser(msg, opts);
+	return {
+		...result,
+		raw: message // Keep the original message
+	};
 };
